c1ed831b61ea4901a1d370cbafb496e470eabae4
federicovalenso <federicovalenso@gmail.com>
2018-10-26 11:07:50 +0800

    Исправлен баг при показе формы для ввода новой категории

--- resources/views/admin/categories/form.blade.php
+++ resources/views/admin/categories/form.blade.php
@@ -1,3 +1,3 @@
 <label for="name">Наименование</label>
-<input type="text" class="w3-input w3-border w3-hover-sand form-control {{ $errors->has('name') ? ' w3-red' : '' }}" placeholder="Введите наименование" name="name" value="{{ old('name') ?? $category->name }}" required>
+<input type="text" class="w3-input w3-border w3-hover-sand form-control {{ $errors->has('name') ? ' w3-red' : '' }}" placeholder="Введите наименование" name="name" value="{{ $category->name ?? old('name') }}" required>
 <button type="submit" class="w3-button w3-teal w3-margin">Сохранить</button>
